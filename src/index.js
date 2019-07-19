8e94cacb154a987e624bdd6f978842277b685155
bebopvinh <vinhdo07@gmail.com>
2019-07-19 12:26:01 -0700

    Add redux tools to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
 
@@ -11,7 +11,9 @@ import App from './App';
 import RestaurantsReducer from './reducers/RestaurantsReducer'
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(RestaurantsReducer, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(RestaurantsReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
    <Provider store={store}>
