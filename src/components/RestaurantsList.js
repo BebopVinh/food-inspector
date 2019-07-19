45a0ba5ecebf92c5fa4ec02a56d203748f5804ca
bebopvinh <vinhdo07@gmail.com>
2019-07-19 14:00:38 -0700

    Add method to replace elements on loaction change

--- src/components/RestaurantsList.js
+++ src/components/RestaurantsList.js
@@ -4,13 +4,16 @@ import Restaurant from '../components/Restaurant'
 
 export class RestaurantsList extends Component {
 
-   renderRestaurants = () => {
-      
+   renderRestaurants = () => {  
       return this.props.restaurants.map(r =>
          <Restaurant restaurant={r} key={r.id} />
       )
    }
 
+   componentWillUpdate(nextProps, nextState) {
+      document.querySelector('div.restaurants-list').innerHTML = ""
+   }
+   
    render() {
       return (
          <div className="restaurants-list">
