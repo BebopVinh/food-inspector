69e5d5b6b0f1ab33338f9a01a786ccba61f3e5ac
bebopvinh <vinhdo07@gmail.com>
2019-07-19 12:25:46 -0700

    Add restaurants array in default state

--- src/reducers/RestaurantsReducer.js
+++ src/reducers/RestaurantsReducer.js
@@ -1,22 +1,20 @@
 export default function manageRestaurants (
-   state = {loading: false}, action
+   state = {loading: false, restaurants: []}, action
 ) {
    switch (action.type) {
       case "LOADING":
          return  {...state, loading: true}
       case "GET_RESTAURANTS":
-         debugger
          const newRestaurants = []
          action.restaurants.forEach(r => {
             const restaurant = {
-               id: r.id,
-               name: r.name,
-               location: r.location,
-               url: r.url
+               id: r.restaurant.id,
+               name: r.restaurant.name,
+               location: r.restaurant.location,
+               url: r.restaurant.url
             }
             newRestaurants.push(restaurant)
          })
-
          return {...state, loading: false, restaurants: state.restaurants.concat(newRestaurants)}
       default:
          return state
