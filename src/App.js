5c5193b25c47a96a07831603c0f122a1d15dd754
bebopvinh <vinhdo07@gmail.com>
2019-07-19 19:06:45 -0700

    Remove RestaurantsContainer from render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import RestaurantsContainer from './containers/RestaurantsContainer';
 
 function App() {
   return (
     <div className="App">
-      <RestaurantsContainer />
+
     </div>
   );
 }
