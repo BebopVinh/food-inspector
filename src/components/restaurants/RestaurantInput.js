fd48c0e8f104efa99d9053d2396f92f17791b5a4
BebopVinh <vinhdo07@gmail.com>
2019-07-22 20:20:48 -0700

    WIP: return restaurants based on location input

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,47 +1,30 @@
 import React, { Component } from 'react'
+import { GoogleComponent } from 'react-google-location'
+
 import { connect } from 'react-redux';
 import {fetchRestaurants} from '../../actions/restaurants'
 
 export class RestaurantInput extends Component {
 
    state = {
-      location: ""
+      place: null
    }
 
    handleChange = (event) => {
-      this.setState({
-         location: event.target.value
-      })
+      // this.setState({
+      //    [event.target.name]: event.target.value
+      // })
+      this.setState({place: event})
    }
 
    handleSubmit = (event) => {
       event.preventDefault()
-      this.props.fetchRestaurants(this.state)
    }
 
    render() {
       return (
          <div>
-            <form onSubmit={this.handleSubmit}>
-               <p>Search by Location: </p>
-               {/* <input type="text" name="location" onChange={this.handleChange} value={this.state.location}/> */}
-               <label>
-                  <input type="radio" name="opt1" value="37.7749, -122.4194" onChange={this.handleChange}/>
-                  San Francisco
-               </label>
-               <br/>
-               <label>
-                  <input type="radio" name="opt1" value="29.9511, -90.0715" onChange={this.handleChange}/>
-                  New Orleans
-               </label>
-               <br/>
-               <label>
-                  <input type="radio" name="opt1" value="41.8781, -87.6298" onChange={this.handleChange}/>
-                  Chicago
-               </label>
-               <br/>
-               <input type="submit" value="Search Restaurants"/>
-            </form>
+            
          </div>
       )
    }
