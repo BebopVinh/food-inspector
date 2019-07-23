4ec5ba3133f188d55a34d9b121b3ef51e9ba3cc0
BebopVinh <vinhdo07@gmail.com>
2019-07-22 20:20:20 -0700

    App to render only Restaurants Container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import RecipesContainer from './containers/RecipesContainer'
+import RestaurantsContainer from './containers/RestaurantsContainer'
 
 
 function App() {
   return (
     <div className="App">
-      <RecipesContainer />
+      {/* <RecipesContainer /> */}
+      <RestaurantsContainer />
     </div>
   );
 }
