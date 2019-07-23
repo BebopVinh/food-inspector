28974033b716cc56c6c3e0a71c8e6a6db18cc651
bebopvinh <vinhdo07@gmail.com>
2019-07-22 22:03:47 -0700

    Form to submit location to fetchRestaurants

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { GoogleComponent } from 'react-google-location'
 
 import { connect } from 'react-redux';
 import {fetchRestaurants} from '../../actions/restaurants'
@@ -7,24 +6,27 @@ import {fetchRestaurants} from '../../actions/restaurants'
 export class RestaurantInput extends Component {
 
    state = {
-      place: null
+      location: null
    }
 
    handleChange = (event) => {
-      // this.setState({
-      //    [event.target.name]: event.target.value
-      // })
-      this.setState({place: event})
+      this.setState({location: event.target.value})
    }
 
    handleSubmit = (event) => {
       event.preventDefault()
+      this.props.fetchRestaurants(this.state)
    }
 
    render() {
       return (
          <div>
-            
+            <form onSubmit={this.handleSubmit}>
+               <label htmlFor="">Search by Location: </label>
+               <input type="text" name="location" value={this.state.location} onChange={this.handleChange}/>
+
+               <input type="submit" value="Search!"/>
+            </form>
          </div>
       )
    }
