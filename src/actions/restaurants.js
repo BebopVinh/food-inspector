8d906efa3ef343349f961b59d519441f23ee724c
bebopvinh <vinhdo07@gmail.com>
2019-07-22 22:03:30 -0700

    fetch POST request to API for Geocoding and Restaurants results

--- src/actions/restaurants.js
+++ src/actions/restaurants.js
@@ -1,37 +1,15 @@
-// Geocode.enableDebug(true)
-
-// const url = ``
-
-// export const fetchRestaurants = (address) => {
-//    const coords = convertToGeo(address)
-//    console.log(coords)
-
-//    // return function (dispatch) {
-//    //    dispatch({type: "LOADING"})
-//    //    return fetch(url, {
-//    //       method: 'GET',
-//    //       headers: {
-//    //          'Content-Type': 'application/json'
-//    //       },
-//    //    })
-//    //    .then(resp => resp.json())
-//    //    .then(json => dispatch({type: "GET_RESTAURANTS", restaurants: json}))
-//    // }
-// }
-
-// const convertToGeo = (address) => {
-//    return dispatch => {
-//       dispatch({type: "LOADING"})
-//       return Geocode.fromAddress(address).then(
-//          response => {
-//             dispatch({type: "LOADING", response})
-//             const { lat, lon } = response.results[0].geometry.location;
-//             console.log(response, lat, lon);
-//             return [lat, lon]
-//          },
-//          error => {
-//             console.error(error);
-//          }
-//       )
-//    }
-// }
+export const fetchRestaurants = (location) => {
+   return dispatch => {
+      dispatch({type: "LOADING"})
+      const url = `http://localhost:3000/restaurants`
+      return fetch(url, {
+         method: 'POST',
+         headers: {
+            "Content-Type": "application/json"
+         },
+         body: JSON.stringify(location)
+      })
+      .then(resp => resp.json())
+      .then(restaurants => dispatch({type: "GET_RESTAURANTS", restaurants}))
+   }
+}
