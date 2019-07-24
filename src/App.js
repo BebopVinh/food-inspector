a0af497c460c04a81d5f754775ba527cd375ab3a
bebopvinh <vinhdo07@gmail.com>
2019-07-24 02:13:31 -0700

    Add Router and Switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
 import './App.css'
 import RecipesContainer from './containers/RecipesContainer'
 import RestaurantsContainer from './containers/RestaurantsContainer'
+import Restaurant from './components/restaurants/Restaurant'
 
 
 function App() {
   return (
     <div className="App">
-      <RecipesContainer />
-      <RestaurantsContainer />
+      <Switch>
+        <Route exact path="/restaurants/:restaurant_id" component={Restaurant}/>
+        <Route exact path="/recipes/:name" component={RecipesContainer} />
+        <Route exact path="/recipes" component={RecipesContainer} />
+        <Route exact path="/" component={RestaurantsContainer}/>
+      </Switch>
     </div>
   );
 }
