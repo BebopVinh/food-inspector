c95a4454f6ada6ee7464f85c65f20100ea7dbc82
bebopvinh <vinhdo07@gmail.com>
2019-07-24 14:33:39 -0700

    Add AllergenReducer

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -1,11 +1,13 @@
 import {combineReducers} from 'redux'
 import RecipesReducer from './RecipesReducer'
 import RestaurantsReducer from './RestaurantsReducer'
+import AllergenReducer from './AllergenReducer'
 
 const rootReducer = combineReducers(
    {
       recipes: RecipesReducer,
-      restaurants: RestaurantsReducer
+      restaurants: RestaurantsReducer,
+      allergen: AllergenReducer
    }
 )
 
