28265239a3de0a4d7ef4f9b55b5358e393e2b62f
bebopvinh <vinhdo07@gmail.com>
2019-07-24 14:39:26 -0700

    App to render AllergenInput

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import './App.css'
 import RecipesContainer from './containers/RecipesContainer'
 import RestaurantsContainer from './containers/RestaurantsContainer'
 import Restaurant from './components/restaurants/Restaurant'
+import AllergenInput from './components/AllergenInput';
 
 
 function App() {
+
   return (
-    <div className="App">
+    <React.Fragment>
+      <AllergenInput />
       <Switch>
         <Route exact path="/restaurants/:restaurant_id" component={Restaurant}/>
         <Route exact path="/recipes/:name" component={RecipesContainer} />
         <Route exact path="/recipes" component={RecipesContainer} />
         <Route exact path="/" component={RestaurantsContainer}/>
       </Switch>
-    </div>
+    </React.Fragment>
   );
 }
 
