684ab595032e851c1a2c08ed73e7d7eae6a2a441
bebopvinh <vinhdo07@gmail.com>
2019-07-24 02:13:55 -0700

    Add component for users to specify allergies

---
+++ src/components/AllergenInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class AllergenInput extends Component {
+   render() {
+      return (
+         <div>
+            
+         </div>
+      )
+   }
+}
+
+export default AllergenInput
