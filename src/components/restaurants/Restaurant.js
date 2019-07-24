cfbf76751407ad1ad34fb34a439c8a082afa7edd
bebopvinh <vinhdo07@gmail.com>
2019-07-23 18:58:08 -0700

    Add more HTML/content to render.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 
 export class Restaurant extends Component {
+
+   renderMenus = (sections) => {
+      return sections.map(section => {
+         return <div key={section.position}>
+            <h4>{section.section_name}</h4>
+            <ul className="menu-items-list">
+               {section.menu_items.map(item => <li>{item.name}</li>)}
+            </ul>
+            <br/>
+         </div>
+      })
+   }
+
    render() {
       const r = this.props.restaurant
       return (
          <div>
-            <a href={`${r.url}`}>{r.name}</a>
+            <h3>{r.restaurant_name}</h3>
+            <ul>
+               {this.renderMenus(r.menus[0].menu_sections)}
+            </ul>
          </div>
       )
    }
