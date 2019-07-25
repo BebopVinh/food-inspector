1988b761fa7b649c947161e75f4d04cdeb7a55fa
BebopVinh <vinhdo07@gmail.com>
2019-07-25 12:54:02 -0700

    Routes to additional component for SignUp and view Profile
    Profile now handles input for Allergen

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import './App.css'
 import RecipesContainer from './containers/RecipesContainer'
 import RestaurantsContainer from './containers/RestaurantsContainer'
 import Restaurant from './components/restaurants/Restaurant'
-import AllergenInput from './components/AllergenInput';
+import UserSignUp from './components/UserSignUp'
+import UserProfile from './components/UserProfile'
 
 
 function App() {
 
   return (
     <React.Fragment>
-      <AllergenInput />
       <Switch>
-        <Route exact path="/restaurants/:restaurant_id" component={Restaurant}/>
+        <Route exact path="/restaurants/:restaurant_id" component={Restaurant} />
         <Route exact path="/recipes/:name" component={RecipesContainer} />
         <Route exact path="/recipes" component={RecipesContainer} />
+
+        <Route exact path="/sign_up" component={UserSignUp}/>
+        {/* <Route exact path="/sign_in" component={UserSignIn}/> */}
+        <Route exact path="/user/profile" component={UserProfile} />
+
         <Route exact path="/" component={RestaurantsContainer}/>
       </Switch>
     </React.Fragment>
