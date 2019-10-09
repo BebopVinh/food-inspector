a3248bbde1dcb06484e2ccefb241fd4b1bbc08f1
BebopVinh <vinhdo07@gmail.com>
2019-10-08 18:22:39 -0700

    Remove backend route for restaurants fetch, using direct API route

--- src/actions/restaurants.js
+++ src/actions/restaurants.js
@@ -1,15 +1,29 @@
 export const fetchRestaurants = (location) => {
+   // return dispatch => {
+   //    dispatch({type: "LOADING"})
+   //    const url = `http://localhost:3000/restaurants`
+   //    return fetch(url, {
+   //       method: 'POST',
+   //       headers: {
+   //          "Content-Type": "application/json"
+   //       },
+   //       body: JSON.stringify(location)
+   //    })
+   //    .then(resp => resp.json())
+   //    .then(restaurants => dispatch({type: "GET_RESTAURANTS", restaurants}))
+   // }
+   debugger
    return dispatch => {
       dispatch({type: "LOADING"})
-      const url = `http://localhost:3000/restaurants`
+      const url = `https://us-restaurant-menus.p.rapidapi.com/restaurants/search`
       return fetch(url, {
-         method: 'POST',
+         method: 'GET',
+         body: JSON.stringify({"lat": location.lat, "lon": location.lon}),
          headers: {
-            "Content-Type": "application/json"
-         },
-         body: JSON.stringify(location)
-      })
-      .then(resp => resp.json())
-      .then(restaurants => dispatch({type: "GET_RESTAURANTS", restaurants}))
+            "Content-Type": "application/json",
+            "X-RapidAPI-Host": "us-restaurant-menus.p.rapidapi.com",
+            "X-RapidAPI-Key": "a65e0a6ec9mshe629e4a46efeb13p1f5432jsn0b66f46daafb"
+         }
+      }).then(resp => console.log(resp))
    }
 }
