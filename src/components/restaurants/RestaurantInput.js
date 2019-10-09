7326a8ef4fcab8b6d23250f11059ee2ba3c1bdb0
BebopVinh <vinhdo07@gmail.com>
2019-10-09 16:26:12 -0700

    Prettier extension autoformatting

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,48 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 import Geocode from "react-geocode"
 
-import { connect } from 'react-redux';
-import {fetchRestaurants} from '../../actions/restaurants'
+import { connect } from "react-redux"
+import { fetchRestaurants } from "../../actions/restaurants"
 
 Geocode.setApiKey(process.env.REACT_APP_GOOGLE_GEOCODE_API_KEY)
 
 export class RestaurantInput extends Component {
+	state = {
+		location: null
+	}
 
-   state = {
-      location: null
-   }
+	handleChange = event => {
+		this.setState({ location: event.target.value })
+	}
 
-   handleChange = (event) => {
-      this.setState({location: event.target.value})
-   }
+	handleSubmit = event => {
+		event.preventDefault()
+		Geocode.fromAddress(this.state.location)
+			.then(
+				response => {
+					const { lat, lng } = response.results[0].geometry.location
+					return { lat, lng }
+				},
+				error => {
+					console.error(error)
+				}
+			)
+			.then(location => {
+				this.props.fetchRestaurants(location)
+			})
+	}
 
-   handleSubmit = (event) => {
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<label>Search by Location: </label>
+					<input
+						type="text"
+						name="location"
+						value={this.state.location}
+						onChange={this.handleChange}
+					/>
 
-      event.preventDefault()
-      Geocode.fromAddress(this.state.location).then(
-         response => {
-            const { lat, lng } = response.results[0].geometry.location
-            console.log(lat, lng)
-         },
-         error => {
-            console.error(error)
-         }
-      )
-      // this.props.fetchRestaurants({location: latLng})
-   }
-
-   render() {
-      return (
-         <div>
-            <form onSubmit={this.handleSubmit}>
-               <label>Search by Location: </label>
-               <input type="text" name="location" value={this.state.location} onChange={this.handleChange}/>
-
-               <input type="submit" value="Search!"/>
-            </form>
-         </div>
-      )
-   }
+					<input type="submit" value="Search!" />
+				</form>
+			</div>
+		)
+	}
 }
 
-export default connect(null, {fetchRestaurants})(RestaurantInput)
+export default connect(
+	null,
+	{ fetchRestaurants }
+)(RestaurantInput)
--- src/reducers/RestaurantsReducer.js
+++ src/reducers/RestaurantsReducer.js
@@ -1,12 +1,14 @@
-export default function manageRestaurants (
-   state = {loading: false, restaurants: []}, action
+export default function manageRestaurants(
+	state = { loading: false, restaurants: [] },
+	action
 ) {
-   switch (action.type) {
-      case "LOADING":
-         return  {...state, loading: true}
-      case "GET_RESTAURANTS":
-         return {...state, loading: false, restaurants: action.restaurants}
-      default:
-         return state
-   }
+	switch (action.type) {
+		case "LOADING":
+			return { ...state, loading: true }
+		case "GET_RESTAURANTS":
+			debugger
+			return { ...state, loading: false, restaurants: action.restaurants }
+		default:
+			return state
+	}
 }
