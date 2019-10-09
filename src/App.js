4714a5ca4a44ac11d1f7bb8d4fe01600f7bed5eb
BebopVinh <vinhdo07@gmail.com>
2019-10-09 16:25:37 -0700

    Syntax fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 import RecipesContainer from './containers/RecipesContainer'
 import RestaurantsContainer from './containers/RestaurantsContainer'
 import Restaurant from './components/restaurants/Restaurant'
-import AllergenInput from './components/AllergenInput';
+import AllergenInput from './components/AllergenInput'
 
 
 function App() {
@@ -14,13 +14,13 @@ function App() {
     <React.Fragment>
       <AllergenInput />
       <Switch>
-        <Route exact path="/restaurants/:restaurant_id" component={Restaurant}/>
+        <Route exact path="/restaurants/:restaurant_id" component={Restaurant} />
         <Route exact path="/recipes/:name" component={RecipesContainer} />
         <Route exact path="/recipes" component={RecipesContainer} />
-        <Route exact path="/" component={RestaurantsContainer}/>
+        <Route exact path="/" component={RestaurantsContainer} />
       </Switch>
     </React.Fragment>
-  );
+  )
 }
 
 export default App;
