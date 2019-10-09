cf5a31d0060fc49e3d2791e2c571f235d687752f
BebopVinh <vinhdo07@gmail.com>
2019-10-09 16:25:23 -0700

    Rewriting dispatch to go directly to RapidAPI without backend

--- src/actions/restaurants.js
+++ src/actions/restaurants.js
@@ -1,29 +1,24 @@
-export const fetchRestaurants = (location) => {
-   // return dispatch => {
-   //    dispatch({type: "LOADING"})
-   //    const url = `http://localhost:3000/restaurants`
-   //    return fetch(url, {
-   //       method: 'POST',
-   //       headers: {
-   //          "Content-Type": "application/json"
-   //       },
-   //       body: JSON.stringify(location)
-   //    })
-   //    .then(resp => resp.json())
-   //    .then(restaurants => dispatch({type: "GET_RESTAURANTS", restaurants}))
-   // }
-   debugger
-   return dispatch => {
-      dispatch({type: "LOADING"})
-      const url = `https://us-restaurant-menus.p.rapidapi.com/restaurants/search`
-      return fetch(url, {
-         method: 'GET',
-         body: JSON.stringify({"lat": location.lat, "lon": location.lon}),
-         headers: {
-            "Content-Type": "application/json",
-            "X-RapidAPI-Host": "us-restaurant-menus.p.rapidapi.com",
-            "X-RapidAPI-Key": "a65e0a6ec9mshe629e4a46efeb13p1f5432jsn0b66f46daafb"
-         }
-      }).then(resp => console.log(resp))
-   }
+export const fetchRestaurants = location => {
+	return dispatch => {
+		dispatch({ type: "LOADING" })
+		const url = `https://us-restaurant-menus.p.rapidapi.com/restaurants/search/geo?page=1&lon=${location.lng}&lat=${location.lat}&distance=10`
+		debugger
+		return fetch(url, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				"X-RapidAPI-Host": "us-restaurant-menus.p.rapidapi.com",
+				"X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_RESTAURANT_MENUS
+			}
+		})
+			.then(response => {
+				response.json()
+			})
+			.then(restaurants => {
+				debugger
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	}
 }
