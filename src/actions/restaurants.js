fdfd9e24cb88515bce0928aea3caf0bec0c6b25f
BebopVinh <vinhdo07@gmail.com>
2019-10-10 18:11:40 -0700

    WIP: restaurants fetch action is failing

--- src/actions/restaurants.js
+++ src/actions/restaurants.js
@@ -1,24 +1,46 @@
 export const fetchRestaurants = location => {
 	return dispatch => {
 		dispatch({ type: "LOADING" })
-		const url = `https://us-restaurant-menus.p.rapidapi.com/restaurants/search/geo?page=1&lon=${location.lng}&lat=${location.lat}&distance=10`
+		const url =
+			"https://us-restaurant-menus.p.rapidapi.com/restaurants/search/geo"
+		const lon = location.lng
+		const lat = location.lat
+		const distance = 20
+		const page = 1
+		const full_url =
+			url + `?lon=${lon}&lat=${lat}&distance=${distance}&page=${page}`
+		const request = async () => {
+			const response = await fetch(full_url, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					"X-RapidAPI-Host": "us-restaurant-menus.p.rapidapi.com",
+					"X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_RESTAURANT_MENUS
+				}
+			})
+			const json = await response.json()
+			debugger
+			console.log(json)
+		}
 		debugger
-		return fetch(url, {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-				"X-RapidAPI-Host": "us-restaurant-menus.p.rapidapi.com",
-				"X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_RESTAURANT_MENUS
-			}
-		})
-			.then(response => {
-				response.json()
-			})
-			.then(restaurants => {
-				debugger
-			})
-			.catch(error => {
-				console.log(error)
-			})
+		return request()
+
+		// fetch(full_url, {
+		// 	method: "GET",
+		// 	headers: {
+		// 		"X-RapidAPI-Host": "us-restaurant-menus.p.rapidapi.com",
+		// 		"X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_RESTAURANT_MENUS
+		// 	}
+		// })
+		// 	.then(response => {
+		// 		debugger
+		// 		console.log(response)
+		// 	})
+		// 	.then(data => {
+		// 		console.log(data)
+		// 	})
+		// 	.catch(error => {
+		// 		console.log(error)
+		// 	})
 	}
 }
