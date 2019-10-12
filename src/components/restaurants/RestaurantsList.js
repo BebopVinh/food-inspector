d4a8c9766a3782d9bc24fc37e8c92e93dc37108b
BebopVinh <vinhdo07@gmail.com>
2019-10-11 22:12:43 -0700

    Fixed if logic and destructured props

--- src/components/restaurants/RestaurantsList.js
+++ src/components/restaurants/RestaurantsList.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
-
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
 
 export class RestaurantsList extends Component {
+	renderRestaurants = () => {
+		const { restaurants } = this.props
+		if (!!restaurants)
+			return restaurants.result.data.map(r => {
+				return (
+					<Link
+						to={{
+							pathname: `/restaurants/${r.restaurant_id}`,
+							state: { restaurant: r }
+						}}
+					>
+						{r.restaurant_name}
+						<br />
+					</Link>
+				)
+			})
+	}
 
-   renderRestaurants = () => { 
-      if (!!this.props.restaurants.length > 0)
-      return (
-         this.props.restaurants.map(r => {
-               return <Link to={{
-                  pathname: `/restaurants/${r.restaurant_id}`,
-                  state: {restaurant: r}
-               }}>{r.restaurant_name}<br/>
-               </Link> 
-            }
-         )
-      )
-   }
+	// componentWillUpdate(nextProps, nextState) {
+	// 	document.querySelector("div.restaurants-list").innerHTML = ""
+	// }
 
-   componentWillUpdate(nextProps, nextState) {
-      document.querySelector('div.restaurants-list').innerHTML = ""
-   }
-   
-   render() {
-      return (
-         <div className="restaurants-list">
-            {this.renderRestaurants()}
-         </div>
-      )
-   }
+	render() {
+		return <div className="restaurants-list">{this.renderRestaurants()}</div>
+	}
 }
 
 export default RestaurantsList
