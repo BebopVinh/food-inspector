b0934130c0c1969858528cb831a9187a7024241a
BebopVinh <vinhdo07@gmail.com>
2019-10-11 22:13:04 -0700

    formatting and changed defaultProps

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import RestaurantInput from '../components/restaurants/RestaurantInput';
-import RestaurantsList from '../components/restaurants/RestaurantsList'
-
-
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import RestaurantInput from "../components/restaurants/RestaurantInput"
+import RestaurantsList from "../components/restaurants/RestaurantsList"
 
 class RestaurantsContainer extends Component {
-
-   render() {
-      return (
-         <div>
-            <RestaurantInput />
-            <RestaurantsList restaurants={this.props.restaurants}/>
-         </div>
-      )
-   }
+	render() {
+		return (
+			<div>
+				<RestaurantInput />
+				<RestaurantsList restaurants={this.props.restaurants} />
+			</div>
+		)
+	}
 }
 
 const mapStateToProps = state => {
-   return {
-      restaurants: state.restaurants.restaurants
-   }
+	debugger
+	return {
+		restaurants: state.restaurants.restaurants
+	}
 }
 
 RestaurantsContainer.defaultProps = {
-   restaurants: [],
+	restaurants: null
 }
 
 export default connect(mapStateToProps)(RestaurantsContainer)
