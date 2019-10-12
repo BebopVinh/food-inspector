d9f2ad1e3bd5397c2132ce21e46b4ee42fa46f1b
BebopVinh <vinhdo07@gmail.com>
2019-10-11 17:39:20 -0700

    Prettier extension autoformatting

--- src/actions/restaurants.js
+++ src/actions/restaurants.js
@@ -1,15 +1,20 @@
-export const fetchRestaurants = (location) => {
-   return dispatch => {
-      dispatch({type: "LOADING"})
-      const url = `http://localhost:3000/restaurants`
-      return fetch(url, {
-         method: 'POST',
-         headers: {
-            "Content-Type": "application/json"
-         },
-         body: JSON.stringify(location)
-      })
-      .then(resp => resp.json())
-      .then(restaurants => dispatch({type: "GET_RESTAURANTS", restaurants}))
-   }
+export const fetchRestaurants = location => {
+	return dispatch => {
+		dispatch({ type: "LOADING" })
+		const url = `http://localhost:3000/restaurants`
+		return fetch(url, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(location)
+		})
+			.then(resp => {
+				console.log(resp)
+				resp.json()
+			})
+			.then(restaurants =>
+				dispatch({ type: "GET_RESTAURANTS", restaurants })
+			)
+	}
 }
--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,35 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
-import { connect } from 'react-redux';
-import {fetchRestaurants} from '../../actions/restaurants'
+import { connect } from "react-redux"
+import { fetchRestaurants } from "../../actions/restaurants"
+
+import Geocode from "react-geocode"
+
+Geocode.setApiKey(process.env.REACT_APP_GOOGLE_GEOCODE_API_KEY)
 
 export class RestaurantInput extends Component {
+	state = {
+		location: null
+	}
 
-   state = {
-      location: null
-   }
+	handleChange = event => {
+		this.setState({ location: event.target.value })
+	}
 
-   handleChange = (event) => {
-      this.setState({location: event.target.value})
-   }
+	handleSubmit = event => {
+		event.preventDefault()
+		Geocode.fromAddress(this.state.location)
+			.then(
+				response => {
+					return response.results[0].geometry.location
+				},
+				error => {
+					console.error(error)
+				}
+			)
+			.then(location => {
+				this.props.fetchRestaurants(location)
+			})
+	}
 
-   handleSubmit = (event) => {
-      event.preventDefault()
-      this.props.fetchRestaurants(this.state)
-   }
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<label htmlFor="">Search by Location: </label>
+					<input
+						type="text"
+						name="location"
+						value={this.state.location}
+						onChange={this.handleChange}
+					/>
 
-   render() {
-      return (
-         <div>
-            <form onSubmit={this.handleSubmit}>
-               <label htmlFor="">Search by Location: </label>
-               <input type="text" name="location" value={this.state.location} onChange={this.handleChange}/>
-
-               <input type="submit" value="Search!"/>
-            </form>
-         </div>
-      )
-   }
+					<input type="submit" value="Search!" />
+				</form>
+			</div>
+		)
+	}
 }
 
-export default connect(null, {fetchRestaurants})(RestaurantInput)
+export default connect(
+	null,
+	{ fetchRestaurants }
+)(RestaurantInput)
