4dba574cfde6871b8e85a12870bafee8b7870886
BebopVinh <vinhdo07@gmail.com>
2019-10-27 18:22:31 -0700

    Switched to functional component with React Hooks

--- src/components/recipes/RecipesInput.js
+++ src/components/recipes/RecipesInput.js
@@ -1,32 +1,27 @@
-import React, { Component } from 'react'
+import React, { useState } from "react"
 
-export class RecipesInput extends Component {
+export default function RecipesInput(props) {
+	const [recipe, setRecipe] = useState("")
 
-   state = {
-      recipe: ""
-   }
+	const handleSubmit = event => {
+		event.preventDefault()
+		props.fetchRecipes(recipe)
+	}
 
-   handleChange = event => {
-      this.setState({ [event.target.name]: event.target.value })
-   }
-
-   handleSubmit = event => {
-      event.preventDefault()
-      this.props.fetchRecipes(this.state)
-   }
-   
-
-   render() {
-      return (
-         <div>
-            <h2>Recipes Search Bar</h2>
-            <form onSubmit={this.handleSubmit}>
-               <input name="recipe" type="text" value={this.state.recipe} onChange={this.handleChange}/>
-               <input type="submit" value="Search!"/>
-            </form>
-         </div>
-      )
-   }
+	return (
+		<div>
+			<h2>Recipes Search Bar</h2>
+			<form onSubmit={handleSubmit}>
+				<input
+					name="recipe"
+					type="text"
+					value={recipe}
+					onChange={event => {
+						setRecipe(event.target.value)
+					}}
+				/>
+				<input type="submit" value="Search!" />
+			</form>
+		</div>
+	)
 }
-
-export default RecipesInput
